db1f79a3b76cb028774ca27dd0da510e339f6f85
iness sfd safady <iness.safady@gmail.com>
2022-01-05 17:22:02 +0100

    je t'aime

--- Calculs/Program.cs
+++ Calculs/Program.cs
@@ -54,7 +54,7 @@ namespace Calculs
                         solution = val1 + val2;
                         if (reponse == solution)
                         {
-                            Console.WriteLine("Bravo !");
+                            Console.WriteLine("Bravo !!");
                         }
                         else
                         {
